f7f39538d99506ccaa5b6f33acdcf867872bdd7a
GabrielBaezB <gabooo@live.com>
2023-07-14 00:50:25 +0000

    modificacion para desplegar 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake assets:drop
 bundle exec rake assets:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
